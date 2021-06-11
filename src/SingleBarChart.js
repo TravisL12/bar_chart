2bc2728c3009364522f36302d36dc0c79b752957
Travis Lawrence <travis@instabase.com>
2021-06-11 14:59:54 -0700

    flip chart height

--- src/SingleBarChart.js
+++ src/SingleBarChart.js
@@ -1,24 +1,19 @@
 import * as d3 from 'd3';
-import React, { useRef, useEffect, useCallback } from 'react';
-
-const mainWidth = 800;
-const mainHeight = 400;
-
-const margin = { top: 10, right: 30, bottom: 20, left: 50 };
-const width = mainWidth - margin.left - margin.right;
-const height = mainHeight - margin.top - margin.bottom;
+import React, { useRef, useEffect, useCallback, useMemo } from 'react';
 
 const rectWidth = 50;
 
 function SingleBarChart({ data }) {
   const ref = useRef();
+  const maxHeight = useMemo(() => Math.max.apply(null, data), [data]);
 
   const draw = useCallback(() => {
     d3.select(ref.current)
       .selectAll('rect')
       .data(data)
       .attr('x', (_, i) => i * rectWidth)
-      .attr('height', (d) => d * 10)
+      .attr('y', (d) => maxHeight - d)
+      .attr('height', (d) => d)
       .attr('width', rectWidth)
       .attr('stroke-width', 3)
       .attr('stroke-dasharray', '5 5')
@@ -31,7 +26,7 @@ function SingleBarChart({ data }) {
   }, [data, draw]);
 
   return (
-    <svg width={rectWidth * data.length} height={mainHeight} ref={ref}>
+    <svg width={rectWidth * data.length} height={maxHeight} ref={ref}>
       {data.map((val, idx) => (
         <rect key={`${val}-${idx}`} />
       ))}
--- src/data.js
+++ src/data.js
@@ -7,9 +7,9 @@ export const getData = (count = 20) => {
   for (let i = 0; i < count; i++) {
     data.push({
       time: `2021-05-05 10:00:0${i}`,
-      cat: randomizer(10),
-      dog: randomizer(10),
-      fish: randomizer(10),
+      cat: randomizer(100, 10),
+      dog: randomizer(100, 10),
+      fish: randomizer(100, 10),
     });
   }
   return data;
@@ -18,7 +18,7 @@ export const getData = (count = 20) => {
 export const getSingleData = (count = 20) => {
   const data = [];
   for (let i = 0; i < count; i++) {
-    data.push(randomizer(10));
+    data.push(randomizer(100, 10));
   }
   return data;
 };
