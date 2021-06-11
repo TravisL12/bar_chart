c6a477bceb04fba3c0bdfd281ad9839ae7ff1a15
Travis Lawrence <travis@instabase.com>
2021-06-11 14:47:27 -0700

    progress

--- src/SingleBarChart.js
+++ src/SingleBarChart.js
@@ -8,69 +8,33 @@ const margin = { top: 10, right: 30, bottom: 20, left: 50 };
 const width = mainWidth - margin.left - margin.right;
 const height = mainHeight - margin.top - margin.bottom;
 
+const rectWidth = 50;
+
 function SingleBarChart({ data }) {
-  const ref = useRef();
-
   const draw = useCallback(() => {
-    if (ref.current) {
-      d3.select(ref.current).selectAll('*').remove();
-    }
-
-    const svg = d3.select(ref.current);
-
-    // List of subgroups = header of the csv files = soil condition here
-    var subgroups = Object.keys(data[0]).slice(1);
-
-    // List of groups = species here = value of the first column called group -> I show them on the X axis
-    const groups = data.map((d) => d.time);
-
-    // Add X axis
-    const x = d3.scaleBand().domain(groups).range([0, width]).padding([0.2]);
-    svg
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${height})`)
-      .call(d3.axisBottom(x));
-
-    // Add Y axis
-    const y = d3.scaleLinear().domain([0, 60]).range([height, 0]);
-    svg
-      .append('g')
-      .attr('transform', `translate(${margin.left})`)
-      .call(d3.axisLeft(y));
-
-    // color palette = one color per subgroup
-    const color = d3
-      .scaleOrdinal()
-      .domain(subgroups)
-      .range(['pink', 'magenta', 'purple']);
-
-    //stack the data? --> stack per subgroup
-    const stackedData = d3.stack().keys(subgroups)(data);
-
-    // Show the bars
-    svg
-      .append('g')
-      .selectAll('g')
-      // Enter in the stack data = loop key per key = group per group
-      .data(stackedData)
-      .join('g')
-      .attr('fill', (d) => color(d.key))
+    d3.select('#chart')
       .selectAll('rect')
-      // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data((d) => d)
-      .join('rect')
-      .attr('x', (d) => x(d.data.time))
-      .attr('transform', `translate(${margin.left})`)
-      .attr('y', (d) => y(d[1]))
-      .attr('height', (d) => y(d[0]) - y(d[1]))
-      .attr('width', x.bandwidth());
+      .data(data)
+      .attr('x', (_, i) => i * rectWidth)
+      .attr('height', (d) => d * 10)
+      .attr('width', rectWidth)
+      .attr('stroke-width', 3)
+      .attr('stroke-dasharray', '5 5')
+      .attr('stroke', 'plum')
+      .attr('fill', 'pink');
   }, [data]);
 
   useEffect(() => {
     draw();
   }, [data, draw]);
 
-  return <svg width={mainWidth} height={mainHeight} ref={ref}></svg>;
+  return (
+    <svg width={rectWidth * data.length} height={mainHeight} id="chart">
+      {data.map((val, idx) => (
+        <rect key={`${val}-${idx}`} />
+      ))}
+    </svg>
+  );
 }
 
 export default SingleBarChart;
