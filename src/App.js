ac9fa66cfd2649e3ef39b87959917ca38edf99c5
Travis Lawrence <travis@instabase.com>
2021-06-16 10:46:41 -0700

    finally!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import StackedBarChart from './StackedBarChart';
-import SingleBarChart from './SingleBarChart';
-import { getSingleData, getData } from './data';
-import { useState } from 'react';
-import Graph from './Graph';
+import StackedBarChart from "./StackedBarChart";
+import SingleBarChart from "./SingleBarChart";
+import { getSingleData, getData } from "./data";
+import Graph from "./Graph";
 
 // https://www.d3-graph-gallery.com/graph/barplot_stacked_basicWide.html
 
 const App = () => {
-  const [data, setData] = useState(getSingleData());
-
   return (
     <div className="App">
       <Graph componentProp={StackedBarChart} getData={getData} />
--- src/SingleBarChart.js
+++ src/SingleBarChart.js
@@ -1,5 +1,5 @@
-import * as d3 from 'd3';
-import React, { useRef, useEffect, useMemo } from 'react';
+import * as d3 from "d3";
+import React, { useRef, useEffect } from "react";
 
 const rectWidth = 30;
 
@@ -17,44 +17,55 @@ function SingleBarChart({ data }) {
 
     const yScale = d3.scaleLinear().domain([0, height]).range([height, 0]);
     const svg = d3.select(ref.current);
-
     svg
-      .selectAll('g')
-      .data(data, (d) => d)
-      .join((enter) => {
-        const g = enter.append('g');
-
-        // create bars
-        g.selectAll('rect')
-          .data(data)
-          .join('rect')
-          .attr('x', (_, i) => xScale(i))
-          .attr('y', (d) => yScale(d))
-          .attr('height', (d) => d)
-          .attr('width', rectWidth)
-          .attr('stroke-width', 1)
-          .attr('stroke', 'plum')
-          .attr('fill', 'pink');
-
-        // create labels
-        g.append('text')
-          .text((d) => d)
-          .join('text')
-          .attr('x', (_, i) => xScale(i))
-          .attr('y', (d) => yScale(d))
-          .attr('width', rectWidth)
-          .attr('dominant-baseline', 'hanging');
-
-        return g;
-      });
+      .selectAll("g")
+      .data(data)
+      .join(
+        (enter) =>
+          enter
+            .append("g")
+            .call((g) =>
+              g
+                .append("rect")
+                .attr("x", (_, i) => xScale(i))
+                .attr("y", (d) => yScale(d))
+                .attr("height", (d) => d)
+                .attr("width", rectWidth)
+                .attr("stroke-width", 1)
+                .attr("stroke", "plum")
+                .attr("fill", "pink")
+            )
+            .call((g) =>
+              g
+                .append("text")
+                .text((d) => d)
+                .join("text")
+                .attr("x", (_, i) => xScale(i))
+                .attr("y", (d) => yScale(d))
+                .attr("width", rectWidth)
+                .attr("dominant-baseline", "hanging")
+            ),
+        (update) =>
+          update.call((g) => {
+            g.select("rect")
+              .transition()
+              .attr("y", (d) => yScale(d))
+              .attr("height", (d) => d);
+            g.select("text")
+              .transition()
+              .text((d) => d)
+              .attr("y", (d) => yScale(d));
+          })
+      );
   };
 
   useEffect(() => {
-    d3.select(ref.current).attr('width', width).attr('height', height);
+    d3.select(ref.current).attr("width", width).attr("height", height);
   }, []);
 
   useEffect(() => {
     draw();
+    return () => {};
   }, [data, draw]);
 
   return <svg ref={ref}></svg>;
