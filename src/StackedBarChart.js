c131267f58725f016b6b7630000a31991df96a06
Travis Lawrence <travis.lawrence12@gmail.com>
2021-06-22 14:48:18 -1000

    updates

--- src/StackedBarChart.js
+++ src/StackedBarChart.js
@@ -13,20 +13,16 @@ function StackedBarChart({ data }) {
 
   const draw = useCallback(() => {
     const svg = d3.select(ref.current);
+    svg.append('g').attr('class', 'stacks');
 
-    // List of groups = species here = value of the first column called group -> I show them on the X axis
     const groups = data.map((d) => d.time);
-    // List of subgroups = header of the csv files = soil condition here
     const subgroups = Object.keys(data[0]).slice(1);
-    //stack the data? --> stack per subgroup
     const stackedData = d3.stack().keys(subgroups)(data);
-    // color palette = one color per subgroup
     const color = d3
       .scaleOrdinal()
       .domain(subgroups)
       .range(['pink', 'magenta', 'purple']);
 
-    // Add X axis
     const xAxis = d3
       .scaleBand()
       .domain(groups)
@@ -40,30 +36,41 @@ function StackedBarChart({ data }) {
       .text((d) => d.slice(11))
       .attr('transform', 'translate(-13,26) rotate(-90)');
 
-    // Add Y axis
     const yAxis = d3.scaleLinear().domain([0, 60]).range([height, 0]);
     svg
       .append('g')
       .attr('transform', `translate(${margin.left})`)
       .call(d3.axisLeft(yAxis));
 
-    // Show the bars
     svg
-      .append('g')
-      .selectAll('g')
-      // Enter in the stack data = loop key per key = group per group
+      .selectAll('g.stacks')
+      .selectAll('g.stack')
       .data(stackedData)
-      .join('g')
-      .attr('fill', (d) => color(d.key))
-      .selectAll('rect')
-      // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data((d) => d)
-      .join('rect')
-      .attr('x', (d) => xAxis(d.data.time))
-      .attr('transform', `translate(${margin.left})`)
-      .attr('y', (d) => yAxis(d[1]))
-      .attr('height', (d) => yAxis(d[0]) - yAxis(d[1]))
-      .attr('width', xAxis.bandwidth());
+      .join((enter) => {
+        const g = enter.append('g').attr('class', 'stack');
+        g.append('g')
+          .attr('class', 'bars')
+          .attr('fill', (d) => color(d.key));
+
+        g.select('.bars')
+          .selectAll('rect')
+          .data(
+            (d) => d,
+            (d) => d.data.time
+          )
+          .join((enterRect) =>
+            enterRect
+              .append('rect')
+              .attr('class', 'bar')
+              .attr('x', (d) => xAxis(d.data.time))
+              .attr('transform', `translate(${margin.left})`)
+              .attr('y', (d) => yAxis(d[1]))
+              .attr('height', (d) => yAxis(d[0]) - yAxis(d[1]))
+              .attr('width', xAxis.bandwidth())
+          );
+
+        return g;
+      });
   }, [data]);
 
   useEffect(() => {
